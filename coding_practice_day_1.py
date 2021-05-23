5727e5cdfda725ebc1a2d7e67cc164b5d66ddb1d
Pratiksha Mali <kishorimali17@gmail.com>
2021-05-23 14:53:29 +0530

    added solution

--- coding_practice_day_1.py
+++ coding_practice_day_1.py
@@ -3,10 +3,30 @@ Author: Pratiksha Mali
 Date :  23 May 2021
 Problem Statement : Divisible sum pairs
 """
+'''
 
 month = int(input("enter number"))
 
 list1 = [1,2,3,4,5,6]
 
 x = sum(list1[0:month])
+print(x)
+
+'''
+#Solution for HackerRank subarray division problen
+def subarraydiv(d,m,n:list):
+    """
+    d = day
+    m = month
+    n = number of squares
+    """
+    count = 0
+    for i in range(len(n)):
+        res = sum(n[i:i+m])
+        #print(res)
+        if res == d:
+            count +=1
+    return count
+
+x = subarraydiv(3,2,[1,2,1,3,2])
 print(x)
