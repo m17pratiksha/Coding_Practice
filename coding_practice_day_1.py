e235879b5205cfc1b548db7e4e10a22eee2f265f
Pratiksha Mali <kishorimali17@gmail.com>
2021-05-23 14:20:01 +0530

    decided the logic to solve subarray division problem

---
+++ coding_practice_day_1.py
@@ -0,0 +1,12 @@
+"""
+Author: Pratiksha Mali
+Date :  23 May 2021
+Problem Statement : Divisible sum pairs
+"""
+
+month = int(input("enter number"))
+
+list1 = [1,2,3,4,5,6]
+
+x = sum(list1[0:month])
+print(x)
