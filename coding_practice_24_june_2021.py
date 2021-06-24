efcc52f84de51efb2d7fc7e96e71462d29e86a83
pratiksha mali <pratiksha.mali@d10x.io>
2021-06-24 19:58:22 +0530

    added solution for migration hackerrank problem

--- coding_practice_24_june_2021.py
+++ coding_practice_24_june_2021.py
@@ -6,12 +6,27 @@ Author: Pratiksha Mali
 Date: 24 june 2021
 """
 
-arr =[1,4,4,4,5,3]
+
 
 def migrationofbirds(arr):
     """
     i expect this function should be able to analyse given array in following manner
-    1) find numbers are repeating or not
+    1) compare numbers in given array
+    2) find numbers are repeating or not
     2) if yes , check how many times number is getting repeated
     3) find out smallest number amon those repeating numbers
     """
+    same_type_birds = []
+
+    for i in range(len(arr)):
+        for j in range((i+1),len(arr)):
+
+            if arr[i] == arr[j]:
+                same_type_birds.append(arr[i])
+
+    small_num = min(same_type_birds)
+
+    return small_num
+
+arr =[1,4,4,4,6,7]
+print(migrationofbirds(arr))
