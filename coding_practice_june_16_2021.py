b8eebcb0c170c0c9c7b54d2787bd82fd52efa09f
pratiksha mali <pratiksha.mali@d10x.io>
2021-06-24 18:43:04 +0530

    added function migrationofbirds and added purpose of function

---
+++ coding_practice_june_16_2021.py
@@ -0,0 +1,17 @@
+"""
+Problem Statment: Given an array of bird sightings where every element
+represents a bird type id,determine the id of the most frequently sighted type of more than 1 type has been
+spotted that maximum amount return the smallest of their ids.
+Author: Pratiksha Mali
+Date: 24 june 2021
+"""
+
+arr =[1,4,4,4,5,3]
+
+def migrationofbirds(arr):
+    """
+    i expect this function should be able to analyse given array in following manner
+    1) find numbers are repeating or not
+    2) if yes , check how many times number is getting repeated
+    3) find out smallest number amon those repeating numbers
+    """
