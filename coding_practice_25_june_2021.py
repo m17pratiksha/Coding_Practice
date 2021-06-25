68c45c1fb24be27ac6d375342f26f56fdb26aace
pratiksha mali <pratiksha.mali@d10x.io>
2021-06-25 18:30:27 +0530

    Added solution for Day of the programmer question on hackarrank.

---
+++ coding_practice_25_june_2021.py
@@ -0,0 +1,21 @@
+"""
+problem statment:
+Marie invented a Time Machine and wants to test it by time-traveling to visit Russia on the Day of the Programmer (the 256th day of the year) during a year in the inclusive range from 1700 to 2700.
+From 1700 to 1917, Russia's official calendar was the Julian calendar; since 1919 they used the Gregorian calendar system. The transition from the Julian to Gregorian calendar system occurred in 1918, when the next day after January 31st was February 14th. This means that in 1918, February 14th was the 32nd day of the year in Russia.
+In both calendar systems, February is the only month with a variable amount of days; it has 29 days during a leap year, and 28 days during all other years. In the Julian calendar, leap years are divisible by 4; in the Gregorian calendar, leap years are either of the following:
+Divisible by 400.
+Divisible by 4 and not divisible by 100.
+
+Author : Pratiksha Mali
+
+Date : 25 June 2021
+"""
+
+def dayOfProgrammer(year):
+    # Write your code here
+    if (year == 1918):
+        return '26.09.1918'
+    elif ((year <= 1917) & (year%4 == 0)) or ((year > 1918) & (year%400 == 0 or ((year%4            == 0) & (year%100 != 0)))):
+        return '12.09.%s' %year
+    else:
+        return '13.09.%s' %year
